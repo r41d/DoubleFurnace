cb0c72ad170e4502928de17948f578db702fc1bd
Lennart Buhl <git@hackmate.de>
2024-10-28 21:24:28 +0100

    Remove migrations because the last update was 3 years ago and we had to change so much for Factorio 2.0, that I now consider these even older migrations unnecessary baggage

--- migrations/DoubleFurnace_0.3.0.lua
+++
@@ -1,9 +0,0 @@
--- Unlock both double furnace recipe if double-smelting-tech was researched
-for i, force in pairs(game.forces) do
-  if force.technologies["double-smelting-tech-electric"].researched then
-    force.recipes["double-steel-furnace"].enabled = true
-    force.recipes["double-electric-furnace"].enabled = true
-  end
-end
-
--- Migration of all double-furnace items and entities to double-electric-furnace is done in the 0.3.0 json migration
